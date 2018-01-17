f3cca75a787fc7ec5b11d1b5767b2ce7f72965e2
yrong <yrong1997@gmail.com>
2018-01-17 21:00:59 +0800

    webpack-builder-advanced(1h)

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const WebpackShellPlugin = require('webpack-shell-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+let mods = {};
+fs.readdirSync("node_modules")
+    .filter(x => [".bin"].indexOf(x) === -1)
+    .forEach(mod => {
+        mods[mod] = "commonjs " + mod;
+    });
+
+const devtool='source-map'
+let postbuild_script = process.env['POST_BUILD_SCRIPT']
+let plugins = [
+    new UglifyJSPlugin({
+        sourceMap: devtool && (devtool.indexOf("sourcemap") >= 0 || devtool.indexOf("source-map") >= 0)
+    }),
+    new WebpackShellPlugin({onBuildEnd:[`/bin/bash ${postbuild_script}`]})
+];
+
+module.exports = (entry, packages) => {
+    entry = entry||{}, entry = Object.assign(entry,{server:'./app.js'})
+    plugins = [...plugins,new CopyWebpackPlugin(packages, {ignore: ['*.gitignore']})]
+    return {
+        target: 'node',
+        entry: entry,
+        devtool: devtool,
+        output: {
+            path: path.resolve('./build'),
+            filename: '[name].js'
+        },
+        node: {
+            __filename: true,
+            __dirname: true
+        },
+        externals: mods,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [["env", {"targets": {"node": "current"}}]]
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: plugins
+    };
+}
